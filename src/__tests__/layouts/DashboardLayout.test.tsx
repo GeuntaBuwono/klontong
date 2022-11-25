5bf06df5f42b7568cf5d5fd4e5cd5d88554a9fbf
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2022-11-26 06:21:09 +0700

    test: coverage dashboard layout

--- src/__tests__/layouts/DashboardLayout.test.tsx
+++ src/__tests__/layouts/DashboardLayout.test.tsx
@@ -1,13 +1,21 @@
-import {render, screen} from '@testing-library/react';
 import DashboardLayout from '@layouts/DashboardLayout';
+import {render, screen} from '@testing-library/react';
+import mockRouter from 'next-router-mock';
+import singletonRouter from 'next/router';
 
 describe('Dashboard Layout', () => {
+	beforeEach(() => {
+		mockRouter.setCurrentUrl('/');
+	});
+
 	const testElement = (
 		<DashboardLayout pageTitle="Testing">Testing</DashboardLayout>
 	);
+
 	test('snapshot', () => {
 		expect(testElement).toMatchSnapshot();
 	});
+
 	test('render', () => {
 		render(testElement);
 		const heading = screen.getByRole('heading', {
@@ -15,5 +23,13 @@ describe('Dashboard Layout', () => {
 			level: 1,
 		});
 		expect(heading).toBeInTheDocument();
+	});
+
+	test('detail product', () => {
+		singletonRouter.push('/dashboard/product/[id]');
+
+		render(testElement);
+		const navElement = document.querySelector('nav');
+		expect(navElement?.className).toBe('h-16 px-4 bg-yellow-200 flex');
 	});
 });
