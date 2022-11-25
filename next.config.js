fc677ed3a104636407a8f99e5357e0aa2146fe18
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2022-11-25 22:31:01 +0700

    chore: add nextjs-routes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withRoutes = require('nextjs-routes/config')({
+	outDir: 'types',
+});
+
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 };
 
-module.exports = nextConfig;
+module.exports = withRoutes(nextConfig);
