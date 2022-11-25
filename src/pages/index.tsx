4ea39145c131590a71f01c73005dba714c02cf97
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2022-11-26 06:22:20 +0700

    feat: add redirect to product on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,10 @@ import Link from 'next/link';
 export default function Home() {
 	return (
 		<DashboardLayout isCenterContent pageTitle="Klontong">
-			<Link href="/" className="border-blue-400 border-2 p-2 text-2xl">
+			<Link
+				href="/dashboard/product"
+				className="border-blue-400 border-2 p-2 text-2xl"
+			>
 				<h3>Product</h3>
 			</Link>
 		</DashboardLayout>
