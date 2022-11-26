75abd828501e9e6acc77c5c59f995d3657e47194
Geunta Buwono <25718928+GeuntaBuwono@users.noreply.github.com>
2022-11-26 19:50:14 +0700

    feat: error message on form input

--- src/__tests__/components/FormInput.test.tsx
+++ src/__tests__/components/FormInput.test.tsx
@@ -13,6 +13,18 @@ describe('FormInput', () => {
 		/>
 	);
 
+	const testElementWithError = (
+		<FormInput
+			placeholder="placeholder testing"
+			value={undefined}
+			errorMessage="has an error"
+			id="testing"
+			label="testing"
+			onBlur={jest.fn()}
+			onChange={jest.fn()}
+		/>
+	);
+
 	test('snapshot', () => {
 		expect(testElement).toMatchSnapshot();
 	});
@@ -31,6 +43,12 @@ describe('FormInput', () => {
 			name: 'testing',
 		});
 		expect(testInput).toHaveDisplayValue('');
+	});
+
+	test('has an error', () => {
+		render(testElementWithError);
+		const testInput = screen.getByText('has an error');
+		expect(testInput).toBeTruthy();
 	});
 
 	test('update value', () => {
--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -1,4 +1,9 @@
-import {ChangeEventHandler, FocusEventHandler} from 'react';
+import classNames from 'classnames';
+import {
+	ChangeEventHandler,
+	FocusEventHandler,
+	HTMLInputTypeAttribute,
+} from 'react';
 
 type FormInputProps = {
 	id: string;
@@ -6,6 +11,8 @@ type FormInputProps = {
 	value: string | number | undefined;
 	onChange: ChangeEventHandler<HTMLInputElement>;
 	onBlur: FocusEventHandler<HTMLInputElement>;
+	type?: HTMLInputTypeAttribute;
+	errorMessage?: string | null;
 	placeholder?: string;
 };
 
@@ -16,20 +23,37 @@ export default function FormInput({
 	onChange,
 	onBlur,
 	placeholder,
+	errorMessage,
+	type = 'text',
 }: FormInputProps) {
+	const labelClassName = classNames(
+		'text-gray-700 capitalize',
+		errorMessage ? 'text-red-600' : null,
+	);
+
+	const inputClassName = classNames(
+		'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50',
+		errorMessage ? 'border-red-600' : null,
+	);
+
 	return (
-		<label className="block my-4">
-			<span className="text-gray-700 capitalize">{label}</span>
-			<input
-				id={id}
-				name={id}
-				type="text"
-				className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-				placeholder={placeholder}
-				value={value}
-				onChange={onChange}
-				onBlur={onBlur}
-			/>
-		</label>
+		<div className="my-4">
+			<label className="block">
+				<span className={labelClassName}>{label}</span>
+				<input
+					id={id}
+					name={id}
+					type={type}
+					className={inputClassName}
+					placeholder={placeholder}
+					value={value}
+					onChange={onChange}
+					onBlur={onBlur}
+				/>
+			</label>
+			{errorMessage && (
+				<label className="text-red-600 text-sm">{errorMessage}</label>
+			)}
+		</div>
 	);
 }
